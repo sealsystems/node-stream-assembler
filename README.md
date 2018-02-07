2a2015efc6be70f4f83b9815612b8de1cb48242f
seal-mt <manfred.thein@sealsystems.de>
2018-02-07 10:20:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# @sealsystems/node-stream-assembler
+
+Assemble data from multiple read streams into a single write stream.
+
+## Installation
+
+```bash
+npm install @sealsystems/node-stream-assembler
+```
+
+## Quick start
+
+First you need to add a reference to `@sealsystems/node-stream-assembler` within your application.
+
+```javascript
+const Assembler = require('@sealsystems/node-stream-assembler');
+```
+
+Then you can create an assembler object.
+
+```javascript
+const assembler = new Assembler(readables);
+```
+
+## API
+
+### constructor(readables[, options])
+
+- `readables` &lt;array&gt; | &lt;function&gt; Creates an object of type `Assembler`.
+- `options` &lt;Object&gt; Pipe options
+  - `end` &lt;boolean&gt; End the writer after all readers end. Defaults to true.
+
+The `readables` array has to contain objects implementing the `stream.Readable` interface.
+
+The `readables` function has to be either a synchronous or an `async` function for which the assembler object can `await` for. The function is called without any parameter and has to return the next read stream at each call or null if no read streams left.
+
+### assembler.pipe(writeable)
+
+- `writeable` <stream.Writeable> The destination for writing data.
+
+Throws on error and returns null on success.
+
+The `pipe` function attaches the `writeable` stream to all readable streams one at a time in the order the readable streams are given.
+
+```javascript
+const assembler = new Assembler(readables);
+
+await assembler.pipe(writeable);
+```
---
+++ test/AssemblerTest.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const { PassThrough } = require('stream');
+
+const assert = require('assertthat');
+
+const Assembler = require('../lib/Assembler');
+
+suite('Assembler', () => {
+  test('is a function', async () => {
+    assert.that(Assembler).is.ofType('function');
+  });
+
+  test('sets default for options', async () => {
+    const assembler = new Assembler([]);
+
+    assert.that(assembler.options.end).is.true();
+  });
+
+  test('sets end option to false', async () => {
+    const assembler = new Assembler([], { end: false });
+
+    assert.that(assembler.options.end).is.false();
+  });
+
+  test('sets end option to true', async () => {
+    const assembler = new Assembler([], { end: true });
+
+    assert.that(assembler.options.end).is.true();
+  });
+
+  test('throws error readables argument is missing', async () => {
+    /* eslint-disable no-new */
+    assert.that(() => {
+      new Assembler();
+    }).is.throwing('Readables are missing.');
+    /* eslint-enable no-new */
+  });
+
+  test('throws error if wrong argument type is given', async () => {
+    /* eslint-disable no-new */
+    assert.that(() => {
+      new Assembler({});
+    }).is.throwing('Argument error. Function expected, but got object.');
+    /* eslint-enable no-new */
+  });
+
+  test('creates readables function for array', async () => {
+    const assembler = new Assembler([]);
+
+    assert.that(assembler.readables).is.ofType('function');
+  });
+
+  test('readables function for array returns next element', async () => {
+    const readables = [1, 2];
+    const assembler = new Assembler(readables);
+
+    assert.that(assembler.readables).is.ofType('function');
+    assert.that(assembler.readables()).is.equalTo(1);
+    assert.that(assembler.readables()).is.equalTo(2);
+    assert.that(assembler.readables()).is.null();
+  });
+
+  test('throws error if writeable is missing', async () => {
+    const assembler = new Assembler([]);
+
+    await assert.that(async () => {
+      await assembler.pipe();
+    }).is.throwingAsync('Writeable stream is missing.');
+  });
+
+  test('pipes content to writeable stream', async () => {
+    const readables = [new PassThrough(), new PassThrough()];
+    const writeable = new PassThrough();
+    const assembler = new Assembler(readables);
+
+    setTimeout(() => {
+      readables[1].end('hansi');
+      readables[0].end('hugo&');
+    }, 200);
+    assert.that(await assembler.pipe(writeable)).is.equalTo(null);
+    assert.that(writeable.read().toString()).is.equalTo('hugo&hansi');
+  });
+
+  test('throws error from readable stream', async () => {
+    const readables = [new PassThrough()];
+    const writeable = new PassThrough();
+    const assembler = new Assembler(readables);
+
+    setTimeout(() => {
+      readables[0].emit('error', new Error('hugos error'));
+    }, 150);
+    await assert.that(async () => {
+      await assembler.pipe(writeable);
+    }).is.throwingAsync('hugos error');
+  });
+});
